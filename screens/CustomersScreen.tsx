50bad84b382b3b1414e58ffc87e6e5a47afbd873
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-04 21:06:14 +0100

    adding nativewind, stepzen, firebase, basic file structure

---
+++ screens/CustomersScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const CustomersScreen = () => {
+  return (
+    <View>
+      <Text>CustomersScreen</Text>
+    </View>
+  );
+};
+
+export default CustomersScreen;
