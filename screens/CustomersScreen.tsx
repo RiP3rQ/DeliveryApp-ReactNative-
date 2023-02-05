94b80b971659679aaa7c648cb18f7a381dccc020
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-05 16:49:23 +0100

    stepzen setup

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useState } from "react";
+import { Input } from "@rneui/themed";
+import { View, Text, ScrollView, Image, ActivityIndicator } from "react-native";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { TabStackParamList } from "../navigator/TabNavigator";
+
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const CustomersScreen = () => {
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
+  const [input, setInput] = useState<string>("");
+
   return (
-    <SafeAreaView>
-      <Text>CustomersScreen</Text>
-    </SafeAreaView>
+    <ScrollView className="bg-[#59C1CC] ">
+      <Image
+        source={{ uri: "https://links.papareact.com/3jc" }}
+        className="w-full h-64"
+        PlaceholderContent={<ActivityIndicator />}
+      />
+      <View className="pt-5 pb-0 px-10 bg-white">
+        <Input
+          placeholder="Search by Customers"
+          value={input}
+          onChangeText={setInput}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
